ea36a31c022fd7bad2de4be1af39a9bae6dab4dc
Kelly Lin <lin400@umail.ucsb.edu>
2021-05-07 15:51:33 -0700

    week 5

--- Instagram/AppDelegate.swift
+++ Instagram/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let parseConfig = ParseClientConfiguration {
+                $0.applicationId = "ikHYwMWMH14P2r4zp47ktAUptbnaHXZCKk82gLOM" // <- UPDATE
+                $0.clientKey = "LGZwN4F8acstPgZCxgJF6nJqv0cAhCEQgpzGNqbq" // <- UPDATE
+                $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: parseConfig)
+        
         return true
     }
 
